b0e4a39f260c470fd2e327919dd33e06f8032add
glk1001 <gregg.kay@gmail.com>
2025-01-01 12:10:56 +1100

    Added 'is built' flag to info.

--- fantagraphics-info.py
+++ fantagraphics-info.py
@@ -6,7 +6,7 @@ from typing import List
 from barks_fantagraphics.comic_book import get_abbrev_jpg_page_list, get_safe_title, ComicBook
 from barks_fantagraphics.comics_cmd_args import CmdArgs, CmdArgNames
 from barks_fantagraphics.comics_consts import RESTORABLE_PAGE_TYPES
-from barks_fantagraphics.comics_utils import setup_logging
+from barks_fantagraphics.comics_utils import get_timestamp, get_max_timestamp, setup_logging
 
 
 def get_issue_titles(title_list: List[str]) -> List[str]:
@@ -18,21 +18,57 @@ def get_issue_titles(title_list: List[str]) -> List[str]:
     return issue_ttls
 
 
-def is_upscayled(comic: ComicBook):
+def is_upscayled(comic: ComicBook) -> bool:
     return all_files_exist(comic.get_srce_upscayled_story_files(RESTORABLE_PAGE_TYPES))
 
 
-def is_restored(comic: ComicBook):
+def is_restored(comic: ComicBook) -> bool:
     return all_files_exist(comic.get_srce_restored_story_files(RESTORABLE_PAGE_TYPES))
 
 
-def has_fixes(comic: ComicBook):
+def has_fixes(comic: ComicBook) -> bool:
     mods = [f[1] for f in comic.get_srce_with_fixes_story_files(RESTORABLE_PAGE_TYPES)]
     if any(mods):
         return True
 
     mods = [f[1] for f in comic.get_final_srce_upscayled_story_files(RESTORABLE_PAGE_TYPES)]
     return any(mods)
+
+
+def is_built(comic: ComicBook) -> bool:
+    if not is_restored(comic):
+        return False
+
+    restored_files = comic.get_srce_restored_story_files(RESTORABLE_PAGE_TYPES)
+    max_restored_timestamp = get_max_timestamp(restored_files)
+    zip_file = comic.get_dest_comic_zip()
+    if not os.path.isfile(zip_file):
+        return False
+    zip_file_timestamp = get_timestamp(zip_file)
+
+    if zip_file_timestamp < max_restored_timestamp:
+        logging.debug(f'Zip file is out of date WRT restored files: "{zip_file}".')
+        return False
+
+    series_comic_zip_symlink = comic.get_dest_series_comic_zip_symlink()
+    if not os.path.islink(series_comic_zip_symlink):
+        return False
+    series_comic_zip_symlink_timestamp = get_timestamp(series_comic_zip_symlink)
+
+    if series_comic_zip_symlink_timestamp < zip_file_timestamp:
+        logging.debug(f'Series symlink is out of date WRT zip file: "{series_comic_zip_symlink}".')
+        return False
+
+    year_comic_zip_symlink = comic.get_dest_year_comic_zip_symlink()
+    if not os.path.islink(year_comic_zip_symlink):
+        return False
+    year_comic_zip_symlink_timestamp = get_timestamp(series_comic_zip_symlink)
+
+    if year_comic_zip_symlink_timestamp < zip_file_timestamp:
+        logging.debug(f'Year symlink is out of date WRT zip file: "{year_comic_zip_symlink}".')
+        return False
+
+    return True
 
 
 def all_files_exist(file_list: List[str]) -> bool:
@@ -50,6 +86,7 @@ def get_restored_or_upscayled_flag(comic: ComicBook) -> str:
         flag = "U"
     return flag
 
+
 cmd_args = CmdArgs("Fantagraphics info", CmdArgNames.VOLUME)
 args_ok, error_msg = cmd_args.args_are_valid()
 if not args_ok:
@@ -66,13 +103,25 @@ max_title_len = max([len(title) for title in titles])
 issue_titles = get_issue_titles(titles)
 max_issue_title_len = max([len(issue_title) for issue_title in issue_titles])
 
+title_flags = dict()
 for title, issue_title in zip(titles, issue_titles):
     comic_book = comics_database.get_comic_book(title)
 
     fixes_flag = "F" if has_fixes(comic_book) else " "
+    restored_or_upscayled_flag = get_restored_or_upscayled_flag(comic_book)
+    built_flag = "B" if is_built(comic_book) else " "
+    page_list = ", ".join(get_abbrev_jpg_page_list(comic_book))
+
+    title_flags[title] = (fixes_flag, restored_or_upscayled_flag, built_flag, page_list)
+
+for title, issue_title in zip(titles, issue_titles):
+    fixes_flag = title_flags[title][0]
+    restored_or_upscayled_flag = title_flags[title][1]
+    built_flag = title_flags[title][2]
+    page_list = title_flags[title][3]
 
     print(
         f'Title: "{title:<{max_title_len}}", {issue_title:<{max_issue_title_len}},'
-        f" {fixes_flag} {get_restored_or_upscayled_flag(comic_book)},"
-        f' jpgs: {", ".join(get_abbrev_jpg_page_list(comic_book))}'
+        f" {fixes_flag} {restored_or_upscayled_flag} {built_flag},"
+        f' jpgs: {page_list}'
     )
